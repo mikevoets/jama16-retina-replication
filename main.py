7c0475ebcf3ffcf09b51da38097a66c713190736
Mike Voets <mwhg.voets@gmail.com>
2017-08-22 11:41:31 +0200

    Add convnets library to utils and import from main file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import scipy as sp
@@ -7,7 +6,8 @@ import time
 from datetime import timedelta
 import math
 import os
-from utils.images import get_image_size
+import utils.images as im
+import utils.convnets as cv
 
 # Development
 import pdb
@@ -17,55 +17,6 @@ IMAGE_TRAIN_DIR = './data/train/'
 IMAGE_TRAIN_LABELS = './data/trainLabels.csv'
 
 BATCH_SIZE = 50
-
-
-def read_images(filename_queue, labels_csv_path):
-    # Reading and decoding labels in csv-format
-    csv_reader = tf.TextLineReader()
-    record_defaults = [[''], ['0']]
-    _, csv_content = csv_reader.read(labels_csv_path)
-    label = tf.decode_csv(
-        csv_content, record_defaults=record_defaults)
-
-    # Reading and decoding images in jpeg-format
-    im_reader = tf.WholeFileReader()
-    im_filename, im_content = im_reader.read(filename_queue)
-    image = tf.image.decode_jpeg(im_content)
-    image = tf.cast(image, tf.float32) / 255.
-    return image, label
-
-
-def image_size(directory):
-    first_im_path = os.listdir(directory)[0]
-    return get_image_size(directory + first_im_path)
-
-
-def plot_images(images, cls_true, cls_pred=None):
-    assert len(images) == len(cls_true) == 9
-
-    # Create figure with 3x3 sub-plots
-    fig, axes = plt.subplots(3, 3)
-    fig.subplots_adjust(hspace=0.3, wspace=0.3)
-
-    for i, ax in enumarate(axes.flat):
-        # Plot image
-        ax.imshow(images[i].reshape(img_shape), cmap='binary')
-
-        # Show true and predicted classes
-        if cls_pred is None:
-            xlabel = "True: {0}".format(cls_true[i])
-        else:
-            xlabel = "True: {0}, Pred {1}".format(cls_true[i], cls_pred[i])
-
-        # Show the classes as the label on the x-axis
-        ax.set_xlabel(xlabel)
-
-        # Remove tickets from the plot
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    # Ensure the polot is shown correctly with multiple plots
-    plt.show()
 
 
 def main():
@@ -78,7 +29,7 @@ def main():
     labels_csv_path = tf.train.string_input_producer([IMAGE_TRAIN_LABELS])
 
     # Read the images and labels
-    image, label = read_images(filename_queue, labels_csv_path)
+    image, label = im.read_images(filename_queue, labels_csv_path)
 
     # Convolutional Layer 1
     filter_size1 = 5        # Convolution filters are 5 x 5 pixels
@@ -92,7 +43,7 @@ def main():
     fc_size = 128           # Number of neurons in fully connected layer
 
     # Tuple with height and width of images used to reshape arrays
-    im_shape = image_size(IMAGE_TRAIN_DIR)
+    im_shape = im.image_size(IMAGE_TRAIN_DIR)
 
     # List with height and with of images
     im_size = list(im_shape)
--- utils/images.py
+++ utils/images.py
@@ -1,5 +1,9 @@
 import struct
 import imghdr
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
 
 def get_image_size(fname):
     '''Determine the image type of fhandle and return its size.
@@ -17,7 +21,7 @@ def get_image_size(fname):
             width, height = struct.unpack('<HH', head[6:10])
         elif imghdr.what(fname) == 'jpeg':
             try:
-                fhandle.seek(0) # Read 0xff next
+                fhandle.seek(0)  # Read 0xff next
                 size = 2
                 ftype = 0
                 while not 0xc0 <= ftype <= 0xcf:
@@ -30,8 +34,57 @@ def get_image_size(fname):
                 # We are at a SOFn block
                 fhandle.seek(1, 1)  # Skip `precision' byte.
                 height, width = struct.unpack('>HH', fhandle.read(4))
-            except Exception: #IGNORE:W0703
+            except Exception:   # IGNORE:W0703
                 return
         else:
             return
         return width, height
+
+
+def image_size(directory):
+    first_im_path = os.listdir(directory)[0]
+    return get_image_size(directory + first_im_path)
+
+
+def read_images(filename_queue, labels_csv_path):
+    # Reading and decoding labels in csv-format
+    csv_reader = tf.TextLineReader()
+    record_defaults = [[''], ['0']]
+    _, csv_content = csv_reader.read(labels_csv_path)
+    label = tf.decode_csv(
+        csv_content, record_defaults=record_defaults)
+
+    # Reading and decoding images in jpeg-format
+    im_reader = tf.WholeFileReader()
+    im_filename, im_content = im_reader.read(filename_queue)
+    image = tf.image.decode_jpeg(im_content)
+    image = tf.cast(image, tf.float32) / 255.
+    return image, label
+
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+
+    # Create figure with 3x3 sub-plots
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumarate(axes.flat):
+        # Plot image
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
+        # Show true and predicted classes
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred {1}".format(cls_true[i], cls_pred[i])
+
+        # Show the classes as the label on the x-axis
+        ax.set_xlabel(xlabel)
+
+        # Remove tickets from the plot
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the polot is shown correctly with multiple plots
+    plt.show()
