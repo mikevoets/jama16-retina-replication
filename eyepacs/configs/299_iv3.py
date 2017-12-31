8d73536d67bafa717d78629211c4efa8f49cb672
Mike Voets <mwhg.voets@gmail.com>
2017-12-31 18:58:05 +0100

    Refactor model ensembling

--- eyepacs/configs/299_iv3.py
+++ eyepacs/configs/299_iv3.py
@@ -2,7 +2,7 @@ from config import Config
 
 from tensorflow.contrib.keras.api.keras.models import Model
 from tensorflow.contrib.keras.api.keras.applications.inception_v3 import InceptionV3
-from tensorflow.contrib.keras.api.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.contrib.keras.api.keras.layers import Dense, GlobalAveragePooling2D, Average
 from tensorflow.contrib.keras.api.keras.optimizers import SGD
 
 
@@ -26,15 +26,24 @@ conf = {
     'weight_decay': 5e-4,
 }
 
+base_model = InceptionV3(weights='imagenet', include_top=False)
 
-model = InceptionV3(weights='imagenet', include_top=False)
-
-x = model.output
+x = base_model.output
 x = GlobalAveragePooling2D()(x)
 predictions = Dense(2, activation='sigmoid')(x)
-model = Model(inputs=model.input, outputs=predictions)
+model = Model(inputs=base_model.input, outputs=predictions)
 
 for layer in model.layers:
     layer.trainable = True
 
 config = Config(model=model, conf=conf)
+
+
+def ensemble(models):
+    outputs = [model.outputs[0] for model in models]
+
+    y = Average()(outputs)
+
+    ensemble = Model(inputs=base_model.input, outputs=y, name='ensemble')
+
+    return ensemble
--- retina4.py
+++ retina4.py
@@ -10,10 +10,8 @@ from sklearn.metrics import roc_auc_score
 from sklearn.utils import class_weight
 from PIL import Image
 
-from tensorflow.contrib.keras.api.keras.models import Model
 from tensorflow.contrib.keras.api.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.contrib.keras.api.keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.contrib.keras.api.keras.layers import Average
 
 from dataset import one_hot_encoded
 
@@ -78,75 +76,60 @@ def multiclass_flow_from_directory(flow_from_directory_gen, m_class_getter):
     for x, y in flow_from_directory_gen:
         yield x, m_class_getter(y)
 
+
+# Get config and set directories.
+model_module = load_module('eyepacs/configs/299_iv3.py')
+config = model_module.config
+
+# Set locations of dataset.
+eye.data_path = "data/eyepacs/"
+eye.train_pre_subpath = config.get('train_dir')
+eye.val_pre_subpath = config.get('val_dir')
+
+num_epochs = 10
+num_images = find_num_train_images()
+num_val_images = find_num_val_images()
+batch_size = config.get('batch_size_train')
+
+print("Find images...")
+
+train_datagen = ImageDataGenerator(**config.get('augmentation_params'))
+val_datagen = ImageDataGenerator(**config.get('augmentation_params'))
+
+train_generator = train_datagen.flow_from_directory(
+        os.path.join(eye.data_path, eye.train_pre_subpath),
+        target_size=(config.get('width'), config.get('height')),
+        batch_size=batch_size)
+
+validation_generator = val_datagen.flow_from_directory(
+        os.path.join(eye.data_path, eye.val_pre_subpath),
+        target_size=(config.get('width'), config.get('height')),
+        batch_size=batch_size)
+
 if True:
     models = []
-    
+
+    # Load all saved models.
     for i in range(0, 10):
-        config = load_module('eyepacs/configs/299_iv3.py').config
-
         model = config.get_model()
-
         model.load_weights('weights/{0}-{1}.hdf5'
                            .format(config.get('name'), i))
-        
         models.append(model)
 
-    outputs = [model.outputs[0] for model in models]
-    y = Average()(outputs)
+    ensemble = model_module.ensemble(models)
+    ensemble.compile(**config.get('compile_params'))
 
-    model = Model(models[0].input, y, name='ensemble')
-    model.compile(**config.get('compile_params'))
-
-    eye.data_path = "data/eyepacs/"
-    eye.train_pre_subpath = config.get('train_dir')
-    eye.val_pre_subpath = config.get('val_dir')
-
-    val_datagen = ImageDataGenerator(**config.get('augmentation_params'))
-    validation_generator = val_datagen.flow_from_directory(                                                                       
-            os.path.join(eye.data_path, eye.val_pre_subpath),                                                                     
-            target_size=(config.get('width'), config.get('height')),                                                              
-            batch_size=config.get('batch_size_train'))                                  
-
-
-    loss = model.evaluate_generator(
-            multiclass_flow_from_directory(validation_generator, transform_target),
-            steps=find_num_val_images() // config.get('batch_size_train'),
-    )
+    loss = ensemble.evaluate_generator(
+        multiclass_flow_from_directory(validation_generator,
+                                       transform_target),
+        steps=find_num_val_images() // config.get('batch_size_train'))
 
     print(loss)
-    sys.exit()
-
+    sys.exit(0)
 
 for i in range(0, 10):
-    # Get config and set directories.
-    config = load_module('eyepacs/configs/299_iv3.py').config
-
-    # Set locations of dataset.
-    eye.data_path = "data/eyepacs/"
-    eye.train_pre_subpath = config.get('train_dir')
-    eye.val_pre_subpath = config.get('val_dir')
-
-    num_epochs = 10
-    num_images = find_num_train_images()
-    num_val_images = find_num_val_images()
-    batch_size = config.get('batch_size_train')
-
     print("Settings: Num Epochs: {}, Batch Size: {}"
           .format(num_epochs, batch_size))
-    print("Find images...")
-
-    train_datagen = ImageDataGenerator(**config.get('augmentation_params'))
-    val_datagen = ImageDataGenerator(**config.get('augmentation_params'))
-
-    train_generator = train_datagen.flow_from_directory(
-            os.path.join(eye.data_path, eye.train_pre_subpath),
-            target_size=(config.get('width'), config.get('height')),
-            batch_size=batch_size)
-
-    validation_generator = val_datagen.flow_from_directory(
-            os.path.join(eye.data_path, eye.val_pre_subpath),
-            target_size=(config.get('width'), config.get('height')),
-            batch_size=batch_size)
 
     print("Setup model {}...".format(i+1))
 
@@ -167,5 +150,4 @@ for i in range(0, 10):
                                        config.get('name'), i),
                                    monitor='val_loss',
                                    save_weights_only='val_loss',
-                                   save_best_only=True)]
-    )
+                                   save_best_only=True)])
