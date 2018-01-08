d1ce1769174e31cebec01ca4aed5b7fec943dffa
Mike Voets <mwhg.voets@gmail.com>
2018-01-08 21:02:32 +0100

    Add scripts for getting new distribution

---
+++ convert.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import importlib
+import eyepacs.v3 as eye
+import csv
+from preprocess import scale_normalize, resize
+from distutils.dir_util import copy_tree
+
+
+def load_module(mod):
+    return importlib.import_module(mod.replace('/', '.').split('.py')[0])
+
+
+# Get config and set directories.
+module = load_module('eyepacs/configs/299_iv3.py')
+config = module.config
+
+# Set locations of dataset.
+eye.data_path = "/media/mike/Elements/Retina/"
+eye.test_subpath = "orig_test/"
+eye.test_pre_subpath = "orig_test/512/"
+
+
+for pre_subpath in [eye.test_pre_subpath]:
+    eye.print_status("Converting images in {}.".format(pre_subpath))
+
+    for size in [299, 256, 128]:
+        eye.print_status("Converting images to {} pixels.".format(size))
+
+        path = os.path.join(eye.data_path, pre_subpath)
+        new_path = path.replace('512', str(size))
+
+        if os.path.exists(new_path):
+            eye.print_status("Already converted.")
+            next
+
+        # Copy files to new path.
+        copy_tree(path, new_path)
+
+        # Convert the images to size.
+        old_images_paths = eye._get_image_paths(path)
+        images_paths = eye._get_image_paths(images_dir=new_path)
+        resize(images_paths, size=size)
+
+
+sys.exit(0)
+
+
+def _maybe_create(images_dir, labels_path):
+    # Helper function for creating subdirectories.
+    eye.print_status("Creating subdirectories...")
+
+    # Skipping if there are no labels.
+    if not os.path.exists(labels_path):
+        eye.print_status("Skipping {}".format(labels_path))
+        return
+
+    # Skip if subdirectories already have been created.
+    num_directories = len(os.listdir(images_dir))
+    if num_directories > 0 and num_directories <= eye.num_classes:
+        eye.print_status("Subdirectories already created!")
+        return
+
+    # Read labels file.
+    with open(labels_path, 'rt') as r:
+        reader = csv.reader(r, delimiter=",")
+        for i, line in enumerate(reader):
+            # Deduct the image filename and label.
+            im_name = line[0] + '.jpeg'
+            label = line[1]
+
+            # Check if group subdirectory has been made before.
+            # If not, make it.
+            im_path = os.path.join(images_dir, im_name)
+            group_path = os.path.join(images_dir, label)
+
+            if not os.path.exists(group_path):
+                os.makedirs(group_path)
+
+            # Move image to subdirectory.
+            new_im_path = os.path.join(group_path, im_name)
+            try:
+                os.rename(im_path, new_im_path)
+            except FileNotFoundError:
+                eye.print_status("Skipping {}".format(im_name))
+
+
+# Retrieve all data-sets.
+test_images_path = os.path.join(eye.data_path, eye.test_pre_subpath)
+
+# Retrieve all labels.
+test_labels_path = os.path.join(eye.data_path, eye.test_labels_filename)
+
+# Create all subdirectories.
+_maybe_create(test_images_path, test_labels_path)
+
+
+sys.exit(0)
+
+# Find the path where processed images should be uploaded to.
+save_path = os.path.join(eye.data_path, eye.test_pre_subpath)
+
+# Get image path.
+images_dir = os.path.join(eye.data_path, eye.test_subpath)
+
+# Get paths of images that are to be preprocessed.
+image_paths = eye._get_image_paths(images_dir=images_dir)
+
+# Get paths of images that already are preprocessed.
+preprocessed_paths = eye._get_image_paths(images_dir=save_path)
+
+# Get paths of images that yet have to be preprocessed.
+images_to_preprocess = set(eye._base_filename(image_paths)) \
+    - set(eye._base_filename(preprocessed_paths))
+
+# Only continue unless the directory does not exist.
+if len(images_to_preprocess) > 0:
+    eye.print_status("Preprocessing images...")
+
+    # Get the full paths of images.
+    preprocess_fns = [os.path.join(images_dir, im + '.jpeg')
+                      for im in images_to_preprocess]
+
+    # Create directory for preprocessed images if necessary.
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # Preprocess images.
+    scale_normalize(image_paths=preprocess_fns, save_path=save_path,
+                    diameter=512)
+else:
+    eye.print_status("Images already preprocessed.")
---
+++ distribute.py
@@ -0,0 +1,73 @@
+import os
+import argparse
+import sys
+import eyepacs.v3 as eye
+from glob import glob
+from shutil import copyfile
+
+parser = argparse.ArgumentParser(description='Redistribute data set.')
+parser.add_argument("source", help="Pool to take from.")
+parser.add_argument("dest", help="Destination of new data set.")
+parser.add_argument("n0", help="Amount to take from class 0.", type=int)
+parser.add_argument("n1", help="Amount to take from class 1.", type=int)
+parser.add_argument("n2", help="Amount to take from class 2.", type=int)
+parser.add_argument("n3", help="Amount to take from class 3.", type=int)
+parser.add_argument("n4", help="Amount to take from class 4.", type=int)
+
+args = parser.parse_args()
+pool = args.source
+
+requested = []
+class_images = []
+total = 0
+
+for i in range(5):
+    requested.append(args.__getattribute__("n{}".format(i)))
+
+    filename_match = os.path.join(pool, str(i), "*.jpeg")
+    paths = glob(filename_match)
+    class_images.append(paths)
+
+    if requested[i] > len(class_images[i]):
+        raise TypeError("Max images in class {} is {}!"
+                        .format(i, len(class_images[i])))
+
+    total += len(paths)
+
+print("Found {} images!".format(total))
+
+for i in range(5):
+    print(" {} -> {} images".format(i, len(class_images[i])))
+
+train_path = os.path.join(args.dest, 'train')
+test_path = os.path.join(args.dest, 'test')
+
+if os.path.exists(args.dest):
+    print("Destination does already exist!")
+    sys.exit(-1)
+else:
+    os.makedirs(args.dest)
+    os.makedirs(train_path)
+    os.makedirs(test_path)
+
+    for i in range(5):
+        os.makedirs(os.path.join(train_path, str(i)))
+        os.makedirs(os.path.join(test_path, str(i)))
+
+test_train_split = []
+
+for i in range(5):
+    train_split = class_images[i][:requested[i]]
+    test_split = class_images[i][requested[i]:]
+
+    for j in range(len(train_split)):
+        eye.print_status("Copying class {} (train): {} / {}"
+                         .format(i, j, len(train_split)))
+        new_path = train_split[j].replace(pool, train_path)
+        copyfile(train_split[j], new_path)
+
+    for j in range(len(test_split)):
+        eye.print_status("Copying class {} (test): {} / {}"
+                         .format(i, j, len(test_split)))
+        new_path = test_split[j].replace(pool, test_path)
+        copyfile(test_split[j], new_path)
