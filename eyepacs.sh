3c3632d7d91b0ae1d8036e69151d780c7bc8f579
Mike Voets <mwhg.voets@gmail.com>
2018-02-24 14:27:19 +0900

    Rename folder to train and test

--- eyepacs.sh
+++ eyepacs.sh
@@ -46,12 +46,12 @@ shuffle_seed=$(echo "$@" | sed "s/.*--seed=\([0-9]\+\).*/\1/g")
 
 # Replace seed with default seed number if no seed number.
 if ! [[ "$shuffle_seed" =~ ^-?[0-9]+$ ]]; then
-  shuffle_seed=42 
+  shuffle_seed=42
 fi
 
 # Skip unpacking if --redistribute parameter is defined.
-if [ $(echo "$@" | grep -c -- "--redistribute") -eq 0 ]; then  
-  
+if [ $(echo "$@" | grep -c -- "--redistribute") -eq 0 ]; then
+
   # Confirm the Basexx .zip files and annotations .xls files are present.
   train_zip_count=$(find "$eyepacs_dir" -maxdepth 1 -iname "train.zip.00*" | wc -l)
   test_zip_count=$(find "$eyepacs_dir" -maxdepth 1 -iname "test.zip.00*" | wc -l)
@@ -110,15 +110,15 @@ done
 
 echo "Creating directories for data sets"
 rm -rf "$eyepacs_dir/bin2"
-mkdir -p "$eyepacs_dir/bin2/develop/0" "$eyepacs_dir/bin2/develop/1"
-mkdir -p "$eyepacs_dir/bin2/validate/0" "$eyepacs_dir/bin2/validate/1"
+mkdir -p "$eyepacs_dir/bin2/train/0" "$eyepacs_dir/bin2/train/1"
+mkdir -p "$eyepacs_dir/bin2/test/0" "$eyepacs_dir/bin2/test/1"
 
 # Define images for binary (2) class 0.
 bin2_0_cnt=48784
 bin2_0_tr_cnt=40688
 bin2_0=$(
-find "$pool_dir/"[0-1] -iname "*.jpg" | 
-shuf --random-source=<(get_seeded_random "$shuffle_seed") | 
+find "$pool_dir/"[0-1] -iname "*.jpg" |
+shuf --random-source=<(get_seeded_random "$shuffle_seed") |
 head -n "$bin2_0_cnt"
 )
 
@@ -135,27 +135,27 @@ distribute_images()
   xargs -I{} cp "{}" "$4"
 }
 
-echo "Gathering images for development set (0/2)"
-distribute_images "$bin2_0" head "$bin2_0_tr_cnt" "$eyepacs_dir/bin2/develop/0/."
+echo "Gathering images for train set (0/2)"
+distribute_images "$bin2_0" head "$bin2_0_tr_cnt" "$eyepacs_dir/bin2/train/0/."
 
-echo "Gathering images for development set (1/2)"
-distribute_images "$bin2_1" head "$bin2_1_tr_cnt" "$eyepacs_dir/bin2/develop/1/."
+echo "Gathering images for train set (1/2)"
+distribute_images "$bin2_1" head "$bin2_1_tr_cnt" "$eyepacs_dir/bin2/train/1/."
 
-echo "Gathering images for validation set (0/2)" 
-distribute_images "$bin2_0" tail "$(expr $bin2_0_cnt - $bin2_0_tr_cnt)" "$eyepacs_dir/bin2/validate/0/."
+echo "Gathering images for test set (0/2)"
+distribute_images "$bin2_0" tail "$(expr $bin2_0_cnt - $bin2_0_tr_cnt)" "$eyepacs_dir/bin2/test/0/."
 
-echo "Gathering images for validation set (1/2)"
-distribute_images "$bin2_1" tail "$(expr $bin2_1_cnt - $bin2_1_tr_cnt)" "$eyepacs_dir/bin2/validate/1/."
+echo "Gathering images for test set (1/2)"
+distribute_images "$bin2_1" tail "$(expr $bin2_1_cnt - $bin2_1_tr_cnt)" "$eyepacs_dir/bin2/test/1/."
 
 echo "Converting data set to tfrecords..."
 git submodule update --init
 
-python ./create_tfrecords/create_tfrecord.py --dataset_dir="$eyepacs_dir/bin2/develop" \
+python ./create_tfrecords/create_tfrecord.py --dataset_dir="$eyepacs_dir/bin2/train" \
        --tfrecord_filename=eyepacs --num_shards=8 --validation_size=0.2 || \
-    { echo "Submodule not initialized. Run git submodule update --init"; 
+    { echo "Submodule not initialized. Run git submodule update --init";
       exit 1; }
 
-python ./create_tfrecords/create_tfrecord.py --dataset_dir="$eyepacs_dir/bin2/validate" \
+python ./create_tfrecords/create_tfrecord.py --dataset_dir="$eyepacs_dir/bin2/test" \
        --tfrecord_filename=eyepacs --num_shards=4 || exit 1
 
 echo "Done!"
