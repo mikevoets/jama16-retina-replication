86fa7e9a27dd89882f810a82e400254280b79292
Mike Voets <mwhg.voets@gmail.com>
2018-02-24 14:27:11 +0900

    Add option to specify custom test set

--- evaluate.py
+++ evaluate.py
@@ -7,17 +7,19 @@ default_messidor2_dir = "./data/messidor2"
 parser = argparse.ArgumentParser(
                     description="Evaluate performance of trained graph "
                                 "on test data set. "
-                                "You should run this with either -m or -e.")
+                                "Specify --data_dir if you use the -o param.")
 parser.add_argument("-m", "--messidor2", action="store_true",
                     help="evaluate performance on Messidor-2")
 parser.add_argument("-e", "--eyepacs", action="store_true",
                     help="evaluate performance on EyePacs set")
+parser.add_argument("-o", "--other", action="store_true",
+                    help="evaluate performance on your own dataset")
 parser.add_argument("--data_dir", help="directory where data set resides")
 
 args = parser.parse_args()
 
-if bool(args.eyepacs) == bool(args.messidor2):
-    print("Specify either -e or -m!")
+if bool(args.eyepacs) == bool(args.messidor2) == bool(args.other):
+    print("Can only evaluate one data set at once!")
     parser.print_help()
     sys.exit(2)
 
@@ -27,3 +29,7 @@ elif args.eyepacs:
     data_dir = default_eyepacs_dir
 elif args.messidor2:
     data_dir = default_messidor2_dir
+elif args.other and args.data_dir is None:
+    print("Please specify --data_dir.")
+    parser.print_help()
+    sys.exit(2)
